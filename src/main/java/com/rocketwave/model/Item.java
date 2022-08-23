60a4e167fb081657b805cbb4fb6aa24750b43547
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-08-23 17:58:36 -0300

    feat: adicionando model de item

---
+++ src/main/java/com/rocketwave/model/Item.java
@@ -0,0 +1,89 @@
+package com.rocketwave.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Item {
+    @Id
+    @Column(name = "ID_ITEM")
+    private Integer id;
+
+    @Column(name = "SKU")
+    private String sku;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "VALOR_UNITARIO_CENTAVOS")
+    private Long valorUnitario;
+
+    @Column(name = "VALOR_TOTAL_CENTAVOS")
+    private Long valorTotal;
+
+    @Column(name = "QUANTIDADE")
+    private Integer quantidade;
+
+    @ManyToOne
+    @JoinColumn(name="PEDIDO_ID", nullable=false)
+    private Pedido pedido;
+
+
+    public Item() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Long valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Long getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Long valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}

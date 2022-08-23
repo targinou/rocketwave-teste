5d2afc0f719c652b09d56ca5013005557b534817
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-08-23 16:32:04 -0300

    first commit

---
+++ src/main/java/com/rocketwave/RocketwaveTesteApplication.java
@@ -0,0 +1,13 @@
+package com.rocketwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketwaveTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketwaveTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketwave/RocketwaveTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketwaveTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

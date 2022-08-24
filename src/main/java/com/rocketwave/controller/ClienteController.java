44a2d916042ddd1b9f4a5953b32193de0ace3088
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-08-24 10:22:09 -0300

    feat: configurando swagger

--- src/main/java/com/rocketwave/controller/ClienteController.java
+++ src/main/java/com/rocketwave/controller/ClienteController.java
@@ -1,9 +1,14 @@
 package com.rocketwave.controller;
 
+import com.rocketwave.dto.ClienteDTO;
 import com.rocketwave.dto.ResponseDTO;
 import com.rocketwave.exception.ConflictException;
 import com.rocketwave.model.Cliente;
 import com.rocketwave.service.ClienteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +18,19 @@ import java.util.List;
 
 
 @RestController
+@Api("Cliente controller")
 @RequestMapping("/api/cliente")
 public class ClienteController {
 
     @Autowired
     ClienteService clienteService;
 
+    @ApiOperation(value = "Cadastrar um cliente")
+    @ApiResponses(value = { @ApiResponse(code = 201, message = "Cliente cadastrado com sucesso!"),
+            @ApiResponse(code = 422, message = "Deve ser informado (dados do cliente)."),
+            @ApiResponse(code = 422, message = "Email já registrado.")})
     @PostMapping
-    public ResponseEntity<?> salvar(@RequestBody Cliente cliente) {
+    public ResponseEntity<?> salvar(@RequestBody ClienteDTO cliente) {
 
         try {
             return clienteService.salvarCliente(cliente);
@@ -32,13 +42,19 @@ public class ClienteController {
 
     }
 
+    @ApiOperation(value = "Listar clientes")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK")})
     @GetMapping
     public List<Cliente> listar () {
         return clienteService.listarClientes();
     }
 
+    @ApiOperation(value = "Atualizar um cliente")
+    @ApiResponses(value = { @ApiResponse(code = 201, message = "Cliente atualizado com sucesso!"),
+            @ApiResponse(code = 422, message = "Deve ser informado (dados do cliente)."),
+            @ApiResponse(code = 422, message = "Email já registrado.")})
     @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
+    public ResponseEntity<?> atualizar(@PathVariable Integer id, @RequestBody ClienteDTO cliente) {
 
         try {
             return clienteService.atualizarCliente(cliente, id);
@@ -49,6 +65,8 @@ public class ClienteController {
         }
     }
 
+    @ApiOperation(value = "Listar clientes")
+    @ApiResponses(value = { @ApiResponse(code = 204, message = "Cliente removido com sucesso!")})
     @DeleteMapping("/{id}")
     public ResponseEntity<?> excluir (@PathVariable Integer id) {
         try {
--- src/main/java/com/rocketwave/controller/PedidoController.java
+++ src/main/java/com/rocketwave/controller/PedidoController.java
@@ -5,18 +5,26 @@ import com.rocketwave.dto.PedidoRequestDTO;
 import com.rocketwave.dto.ResponseDTO;
 import com.rocketwave.exception.ConflictException;
 import com.rocketwave.service.PedidoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api("Pedido controller")
 @RequestMapping("/api/pedido")
 public class PedidoController {
 
     @Autowired
     PedidoService pedidoService;
 
+    @ApiOperation(value = "Salvar um pedido")
+    @ApiResponses(value = { @ApiResponse(code = 201, message = "Pedido adicionado com sucesso!"),
+            @ApiResponse(code = 422, message = "Deve ser informado (dados do cliente).")})
     @PostMapping
     public ResponseEntity<?> salvar(@RequestBody PedidoRequestDTO pedidoDTO) {
         try {
@@ -28,6 +36,9 @@ public class PedidoController {
         }
     }
 
+    @ApiOperation(value = "Deleta um pedido por id")
+    @ApiResponses(value = { @ApiResponse(code = 204, message = "Pedido removido com sucesso!"),
+            @ApiResponse(code = 400, message = "Pedido não encontrado.")})
     @DeleteMapping("{id}")
     public ResponseEntity<?> excluir (@PathVariable Integer id) {
         try {
